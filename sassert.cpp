dd874f4412bf3768de9145e33d047809e895761e
jonathan poelen <jonathan.poelen@gmail.com>
2013-07-25 00:31:09 +0200

    adding abort() and colors

--- sassert.cpp
+++ sassert.cpp
@@ -3,8 +3,19 @@
 #include <falcon/type_traits/eval_if.hpp>
 #include <falcon/type_traits/is_character.hpp>
 #include <falcon/type_traits/is_string.hpp>
+#include <cstdlib>
+
+#ifndef NO_BOOST
+# include <boost/current_function.hpp>
+#endif
 
 namespace Super_Assert {
+
+#ifdef NDEBUG
+
+#define sassert(msg, expr)
+
+#else
 
 #if defined(SASSERT_COLOR) && !defined(SASSERT_NO_COLOR)
 //TODO SASSERT_DYNAMIC_COLOR (read file configuration)
@@ -29,22 +40,42 @@ namespace Super_Assert {
 #  ifndef SASSERT_COLOR_UNKNOW
 #    define SASSERT_COLOR_UNKNOW "\033[00;36m"
 #  endif
-#  ifndef SASSERT_COLOR_OPERATOR
-#    define SASSERT_COLOR_OPERATOR "\033[00;30m"
+#  ifndef SASSERT_COLOR_SYMBOLE
+#    define SASSERT_COLOR_SYMBOLE "\033[00;35m"
+#  endif
+#  ifndef SASSERT_COLOR_EXPR
+#    define SASSERT_COLOR_EXPR "\033[01;30m"
+#  endif
+#  ifndef SASSERT_COLOR_MSG
+#    define SASSERT_COLOR_MSG "\033[01;31m"
+#  endif
+#  ifndef SASSERT_COLOR_FILE
+#    define SASSERT_COLOR_FILE "\033[01;31m"
+#  endif
+#  ifndef SASSERT_COLOR_LINE
+#    define SASSERT_COLOR_LINE "\033[00;33m"
+#  endif
+#  ifndef SASSERT_COLOR_FUNCTION
+#    define SASSERT_COLOR_FUNCTION "\033[01;34m"
 #  endif
 #  ifndef SASSERT_COLOR_RESET
 #    define SASSERT_COLOR_RESET "\033[00m"
 #  endif
 #else
-#  define SASSERT_COLOR_CHARACTER
-#  define SASSERT_COLOR_FLOATING
-#  define SASSERT_COLOR_INTEGRAL
-#  define SASSERT_COLOR_BOOLEAN
-#  define SASSERT_COLOR_STRING
-#  define SASSERT_COLOR_OTHER
-#  define SASSERT_COLOR_UNKNOW
-#  define SASSERT_COLOR_OPERATOR
-#  define SASSERT_COLOR_RESET
+#  define SASSERT_COLOR_CHARACTER ""
+#  define SASSERT_COLOR_FLOATING ""
+#  define SASSERT_COLOR_INTEGRAL ""
+#  define SASSERT_COLOR_BOOLEAN ""
+#  define SASSERT_COLOR_STRING ""
+#  define SASSERT_COLOR_OTHER ""
+#  define SASSERT_COLOR_UNKNOW ""
+#  define SASSERT_COLOR_SYMBOLE ""
+#  define SASSERT_COLOR_EXPR ""
+#  define SASSERT_COLOR_MSG ""
+#  define SASSERT_COLOR_FILE ""
+#  define SASSERT_COLOR_LINE ""
+#  define SASSERT_COLOR_FUNCTION ""
+#  define SASSERT_COLOR_RESET ""
 #endif
 
 enum class PrinterType
@@ -82,8 +113,8 @@ struct Printer
 
   template<typename U>
   static auto dispatch_print(const U& x, int)
-  -> decltype(std::cout << x)
-  { return std::cout << SASSERT_COLOR_OTHER "{ " << x << " }" SASSERT_COLOR_RESET; }
+  -> decltype(std::cerr << x)
+  { return std::cerr << SASSERT_COLOR_OTHER "{ " << x << " }" SASSERT_COLOR_RESET; }
 
   template<typename U>
   static void dispatch_print(const U& x, unsigned)
@@ -91,45 +122,63 @@ struct Printer
 
   template<typename U>
   static auto dispatch_with_to_string(const U& x, int)
-  -> decltype(std::cout << to_string(x))
-  { return std::cout << SASSERT_COLOR_STRING "\"" << to_string(x) << "\"" SASSERT_COLOR_RESET; }
+  -> decltype(std::cerr << to_string(x))
+  { return std::cerr << SASSERT_COLOR_STRING "\"" << to_string(x) << "\"" SASSERT_COLOR_RESET; }
 
   template<typename U>
   static void dispatch_with_to_string(const U&, unsigned)
-  { std::cout << SASSERT_COLOR_UNKNOW "<unknow>" SASSERT_COLOR_RESET; }
+  { std::cerr << SASSERT_COLOR_UNKNOW "<unknow>" SASSERT_COLOR_RESET; }
 };
 
 template<typename T>
 struct Printer<T, PrinterType::integral>
 {
   static void print(const T& x)
-  { std::cout << "" SASSERT_COLOR_INTEGRAL << x << "" SASSERT_COLOR_RESET; }
+  { std::cerr << "" SASSERT_COLOR_INTEGRAL << x << "" SASSERT_COLOR_RESET; }
 };
 
 template<typename T>
 struct Printer<T, PrinterType::floating_point>
 {
   static void print(const T& x)
-  { std::cout << "" SASSERT_COLOR_FLOATING << x << "" SASSERT_COLOR_RESET; }
+  { std::cerr << "" SASSERT_COLOR_FLOATING << x << "" SASSERT_COLOR_RESET; }
 };
 
 template<typename T>
 struct Printer<T, PrinterType::string>
 {
   static void print(const T& x)
-  { std::cout << SASSERT_COLOR_STRING "\"" << x << "\"" SASSERT_COLOR_RESET; }
+  { dispatch_print(x, 0); }
+
+  static auto dispatch_print(const T& x, int)
+  -> decltype(std::cerr << x)
+  { return std::cerr << SASSERT_COLOR_STRING "\"" << x << "\"" SASSERT_COLOR_RESET; }
+
+  static void dispatch_print(const T& x, unsigned)
+  { std::wcerr << SASSERT_COLOR_STRING "\"" << x << "\"" SASSERT_COLOR_RESET; }
 };
 
 template<typename T>
 struct Printer<T, PrinterType::character>
 {
-  static void print(const T& x)
+  static void print(char x)
   {
-    if (x == std::char_traits<T>::to_char_type('\'')) {
-      std::cout << SASSERT_COLOR_CHARACTER"'\''" SASSERT_COLOR_RESET;
+    if (x == std::char_traits<char>::to_char_type('\'')) {
+      std::cerr << SASSERT_COLOR_CHARACTER"'\''" SASSERT_COLOR_RESET;
     }
     else {
-      std::cout << SASSERT_COLOR_CHARACTER "'" << x << "'" SASSERT_COLOR_RESET;
+      std::cerr << SASSERT_COLOR_CHARACTER "'" << x << "'" SASSERT_COLOR_RESET;
+    }
+  }
+
+  template<typename U>
+  static void print(const U& x)
+  {
+    if (x == std::char_traits<T>::to_char_type('\'')) {
+      std::wcerr << SASSERT_COLOR_CHARACTER"'\''" SASSERT_COLOR_RESET;
+    }
+    else {
+      std::wcerr << SASSERT_COLOR_CHARACTER "'" << x << "'" SASSERT_COLOR_RESET;
     }
   }
 };
@@ -140,10 +189,10 @@ struct Printer<T, PrinterType::boolean>
   static void print(const T& x)
   {
     if (x) {
-      std::cout << SASSERT_COLOR_BOOLEAN "true" SASSERT_COLOR_RESET;
+      std::cerr << SASSERT_COLOR_BOOLEAN "true" SASSERT_COLOR_RESET;
     }
     else {
-      std::cout << SASSERT_COLOR_BOOLEAN "false" SASSERT_COLOR_RESET;
+      std::cerr << SASSERT_COLOR_BOOLEAN "false" SASSERT_COLOR_RESET;
     }
   }
 };
@@ -159,7 +208,7 @@ struct SAssert
   void print() const
   {
     print(l);
-    std::cout << " " SASSERT_COLOR_OPERATOR << op << SASSERT_COLOR_RESET " ";
+    std::cerr << " " SASSERT_COLOR_SYMBOLE << op << SASSERT_COLOR_RESET " ";
     print(r);
   }
 
@@ -231,16 +280,38 @@ MOP(>=)
 #undef MOP
 }
 
-///BOOST_PP_CAT
-#define PP_CAT(e) #e
+//BOOST_PP_CAT
+#define PP_CAT_I(e) #e
+#define PP_CAT(e) PP_CAT_I(e)
 
-#define sassert(s, ...) do {\
-  std::cout << s << "\n" << PP_CAT(( __VA_ARGS__ )) << "\n";\
-  using namespace Super_Assert;\
-  (B * __VA_ARGS__).print();\
-  std::cout << "\n";\
+#ifndef NO_BOOST
+#  define SASSERT_FUNCTION BOOST_CURRENT_FUNCTION
+#elif defined __GNU && defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
+#  define SASSERT_FUNCTION __PRETTY_FUNCTION__
+#else
+#  if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+#    define SASSERT_FUNCTION __func__
+#  else
+#    define SASSERT_FUNCTION ""
+#  endif
+# endif
+
+#define sassert(msg, expr) do {\
+  if ((expr) ? 0 : 1) {\
+    std::ios_base::sync_with_stdio(1);\
+    std::cerr << SASSERT_COLOR_MSG << (msg) << SASSERT_COLOR_RESET ": ";\
+    (Super_Assert::B * expr).print();\
+    std::cerr << \
+    "\n" SASSERT_COLOR_FILE __FILE__ SASSERT_COLOR_RESET \
+    ":" SASSERT_COLOR_LINE PP_CAT(__LINE__) SASSERT_COLOR_RESET \
+    ": " SASSERT_COLOR_FUNCTION << SASSERT_FUNCTION << SASSERT_COLOR_RESET \
+    ": '" SASSERT_COLOR_EXPR PP_CAT(expr) SASSERT_COLOR_RESET "' failed" \
+    << std::endl;\
+    abort();\
+  }\
 } while(0)
 
+#endif
 
 int main()
 {
@@ -252,9 +323,7 @@ int main()
           a * b + c + 'a' < d );
   sassert("non satisfaisant à cause de la priorité des opérateurs",
           a * b && c < d );
-  sassert("ok... :/",
-          a * b &&B* c < d ); //B est une variable...
-  struct A{} aa;
+  struct A{ operator bool (){return true;}} aa;
   sassert("no printable", aa);
   sassert("s==s", ""=="");
 }
